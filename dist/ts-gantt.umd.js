9c3b8e32757d457e309f113089b2b5de0af37615
Володимир Єрмоленко <yermolim@gmail.com>
2021-12-21 20:23:43 +0200

    update packages;

--- dist/ts-gantt.umd.js
+++ dist/ts-gantt.umd.js
@@ -2,7 +2,7 @@
     typeof exports === 'object' && typeof module !== 'undefined' ? factory(exports) :
     typeof define === 'function' && define.amd ? define(['exports'], factory) :
     (global = typeof globalThis !== 'undefined' ? globalThis : global || self, factory(global.tsGantt = {}));
-}(this, (function (exports) { 'use strict';
+})(this, (function (exports) { 'use strict';
 
     class TsGanttConst {
     }
@@ -1575,4 +1575,4 @@
 
     Object.defineProperty(exports, '__esModule', { value: true });
 
-})));
+}));
